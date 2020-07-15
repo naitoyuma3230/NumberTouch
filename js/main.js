a626dbda0b352a69356efe2acb8d45ef05c44305
NantoYuma <naitoyuma@outlook.jp>
2020-07-15 15:02:31 +0900

    level100

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@
   class Panel{
     // board.panelsにはpanelクラスのオブジェクトがpushされる
     // panel.elプロパティ=pressedクラスのli要素を作成
-    constructor(){
+    constructor(game){
+      this.game = game;
       this.el = document.createElement('li');
       this.el.classList.add('pressed');
       this.el.addEventListener('click', ()=>{
@@ -25,24 +26,27 @@
     // 選択されたパネルの正誤判定
     check(){
     // parseIntで文字列を10進法数値に変換 currentNumと一致すれば正解判定
-      if(currentNum === parseInt(this.el.textContent, 10)){
+      if(this.game.getCurrentNum() === parseInt(this.el.textContent, 10)){
         this.el.classList.add('pressed');
-        currentNum ++;
+        this.game.addCurrentNum();
 
         // 4つの数字が全て選択されたらタイマーストップ
-        if(currentNum === 4){
-          clearInterval(timeoutId);
+        if(this.game.getCurrentNum() === this.game.getLevel() ** 2){
+          clearInterval(this.game.getTimeoutId());
         }
       }
     }
   }
 
   class Board{
-    constructor(){
+    // クラス間のプロパティの引き渡し
+    // Gameクラスのプロパティをthis引数で渡す。board側はgame引数で受け取る
+    constructor(game){
+      this.game = game;
       this.panels = [];
-      // Panelクラスをpanelsプロパティに4回Pushする
-      for(let i = 0; i < 4; i++){
-        this.panels.push(new Panel());
+      // Panelクラスをpanelsプロパティにlevel数値の2乗回Pushする
+      for(let i = 0; i < this.game.getLevel() ** 2; i++){
+        this.panels.push(new Panel(this.game));
       }
       this.setup();
     }
@@ -60,23 +64,27 @@
     
     // Panelクラスの同名のメソッドを各Panelに実行するメソッド
     activate(){
-      const nums = [0, 1, 2, 3];
+      const nums = [];
+      // panelに表記する数字をnumsに格納
+      for(let i = 0; i < this.game.getLevel() ** 2; i++){
+        nums.push(i);
+      }
 
       this.panels.forEach(panel =>{
         // nums配列からランダムな位置をspliceで取り出す
         // spliceで取り出した数値は1つでも配列となるので[0]でアクセス
         const num = nums.splice(Math.floor(Math.random() * nums.length), 1)[0];
         panel.activate(num);
+        // panelクラスのメソッドにアクセスしている？
       });
     }
-
   }
     
-  
-
   class Game{
-    constructor(){
-      this.board = new Board();
+    constructor(level){
+      // new game()の引数で渡された数値をlevelプロパティとして保存
+      this.level  = level;
+      this.board = new Board(this);
 
       this.currentNum = undefined;
       this.startTime = undefined;
@@ -86,6 +94,7 @@
       btn.addEventListener('click', ()=>{
         this.start();
       });
+      this.setup();
     }
 
     start(){
@@ -97,7 +106,7 @@
       
         this.currentNum = 0;
         // boardクラスのactivateメソッド
-        board.activate();
+        this.board.activate();
         this.startTime = Date.now();
         this.runTimer();
     }
@@ -113,8 +122,33 @@
         this.runTimer();
       }, 10);
     }
+
+    addCurrentNum(){
+      this.currentNum++;
+    }
+
+    getCurrentNum(){
+      return this.currentNum;
+    }
+
+    getTimeoutId(){
+      return this.timeoutId;
+    }
+
+    getLevel(){
+      return this.level;
+    }
+
+    // レベルに合わせてcontainerの大きさを変更
+    setup(){
+      const container = document.getElementById('container');
+      const PANEL_WIDTH = 50;
+      const BOARD_PADDING = 10;
+      container.style.width = PANEL_WIDTH * this.level + BOARD_PADDING * 2 + 'px';
+    }
+
   }
 
-  new Game();
+  new Game(10);
 
 }
